0ded42c1a72c94018840e46546b8657cdd05ae20
kierzkowskij2 <janekkierzkowski999@gmail.com>
2016-02-27 18:41:33 +0100

    changes

--- Hackaton/Scripts/SkryptJanusza.js
+++ Hackaton/Scripts/SkryptJanusza.js
@@ -2,15 +2,43 @@
 
 function onstart()
 {
-    wczytaj_dane_profilowe();
-}
-
-function wczytaj_dane_profilowe()
-{
     var pole_imie = document.getElementById("imie");
     var pole_nazwisko = document.getElementById("nazwisko");
     var wiek = document.getElementById("wiek");
+    var starehaslo = document.getElementById("stare_haslo");
+    var nowehaslo1 = document.getElementById("nowe_haslo1");
+    var nowehaslo2 = document.getElementById("nowe_haslo2");
+    wczytaj_dane_profilowe();
+    var przycisk = document.getElementById("potwierdz_zmiany_profilu_button");
+    przycisk.onclick(klikniecie());
+
+}
+
+
+
+function wczytaj_dane_profilowe()
+{
+    
 
     // teraz wczytam to do moich pól
+    return $.ajax({
+        url: 'Map/GetTrees', // jakies uri lel
+        type: 'GET',
+        dataType: 'json',
+        cache: false,
+        success: function () {
+            pole_imie.textContent = "meine imie";
+            pole_nazwisko.textContent = "meine nazwisko";
+            wiek.textContent = 22;
+            starehaslo.textContent = "poprzednie haslo 123";
+        },
+        error: function () {
+            alert('Błąd wczytywania użytkownika');
+        }
+    });
+}
 
+function klikniecie()
+{
+    alert('lel kliknąłem');
 }
