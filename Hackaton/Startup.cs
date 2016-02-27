3a67ec6e3b35cf68d5f48a4b9fef68643d30dd2b
bakala12 <bakala12@o2.pl>
2016-02-27 09:55:12 +0100

    initial commit

---
+++ Hackaton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackaton.Startup))]
+namespace Hackaton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
