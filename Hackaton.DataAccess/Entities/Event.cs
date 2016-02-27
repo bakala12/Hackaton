21b4d82ae6e2e968cb4f0c2b98ae302f9261111b
kierzkowskij2 <janekkierzkowski999@gmail.com>
2016-02-27 17:31:58 +0100

    brak kropki

--- Hackaton.DataAccess/Entities/Event.cs
+++ Hackaton.DataAccess/Entities/Event.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Shared;
 
 namespace Hackaton.DataAccess.Entities
 {
