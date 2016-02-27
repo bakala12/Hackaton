74e85bc69b14eb3a6aa3d9e3cad6786901e35a06
Augi <mateeusz94@gmail.com>
2016-02-27 16:33:15 +0100

    dfsfs

---
+++ Services/EventService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hackaton.DataAccess;
+
+namespace Services
+{
+    public class EventService
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+    }
+}
---
+++ Shared/Dtos/EventDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        [Description("Miejsce gdzie odbędzię się wydarzenie")]
+        public virtual TreeDto Tree { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual EventType EventType { get; set; }
+        public DateTime Date { get; set; }
+        public virtual UserDto Organizer { get; set; }
+        public virtual ICollection<UserDto> Participants { get; set; }
+    }
+}
