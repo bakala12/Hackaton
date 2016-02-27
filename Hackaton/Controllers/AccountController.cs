0b42f419cc7cc5824c676591659dbb1840e44875
Sławomir Fajer <soivek@hotmail.com>
2016-02-27 10:55:18 +0100

    testowy commit

--- Hackaton/Controllers/AccountController.cs
+++ Hackaton/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Hackaton.Models;
-
+// testowy commit 
 namespace Hackaton.Controllers
 {
     [Authorize]
