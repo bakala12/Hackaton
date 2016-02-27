6054bfb7ef2d395c6ccda6cafb6c54f98e461554
posejdon <aniutka.pop@gmail.com>
2016-02-27 18:07:53 +0100

    poprawka

--- Hackaton/Scripts/sryptyAni.js
+++ Hackaton/Scripts/sryptyAni.js
@@ -1,21 +1,30 @@
 ﻿$(window).onload(start());
 function start()
 {
-    wydarzenia();
+    getEvents();
 }
-function wydarzenia()
-{
+function getEvents() {
     var wyd = document.getElementById("listaWydarzen");
-    for(var i = 0; i < 5; i ++)
-    {
-        var divik = document.createElement("div");
-        var obrazek = document.createElement("img");
-        obrazek.src = "~/Images/biegacz.png";
-        var data = document.createElement("span");
-        var miejsce = document.createElement("span");
-        divik.appendChild(obrazek);
-        divik.appendChild(data);
-        divik.appendChild(miejsce);
-        wyd.appendChild(divik)
-    }
+    return $.ajax({
+        url: 'Map/GetTrees',
+        type: 'GET',
+        dataType: 'json',
+        cache: false,
+        success: function (results) {
+            for (var i = 0; i < results.length; i++) {
+                var divik = document.createElement("div");
+                var obrazek = document.createElement("img");
+                obrazek.src = "~/Images/biegacz.png";
+                var data = document.createElement("span");
+                var miejsce = document.createElement("span");
+                divik.appendChild(obrazek);
+                divik.appendChild(data);
+                divik.appendChild(miejsce);
+                wyd.appendChild(divik)
+            }
+        },
+        error: function () {
+            alert('Error occured');
+        }
+    });
 }
