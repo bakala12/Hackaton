87a761493037eb024e2b883d55e9029ddc5bd765
bakala12 <bakala12@o2.pl>
2016-02-28 17:41:02 +0100

    Last

--- Hackaton/Models/ManageViewModels.cs
+++ Hackaton/Models/ManageViewModels.cs
@@ -43,17 +43,17 @@ namespace Hackaton.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Aktualne hasło")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nowe hasło")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Potwierdź")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
--- Shared/Dtos/EventDto.cs
+++ Shared/Dtos/EventDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace Shared.Dtos
         public int Id { get; set; }
         [Description("Miejsce gdzie odbędzię się wydarzenie")]
         public virtual TreeDto Tree { get; set; }
+        [Display(Name = "Imię")]
         public string Name { get; set; }
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+        [Display(Name = "Typ biegu")]
         public virtual EventType EventType { get; set; }
+        [Display(Name = "Data")]
         public DateTime Date { get; set; }
         public virtual UserDto Organizer { get; set; }
         public virtual ICollection<UserDto> Participants { get; set; }
